1e1068c5559c9504f3c8fc1c1a951564790212b9
Jean-baptiste PINIAU <jpiniau@e3r8p4.42.fr>
2016-01-30 16:32:39 +0100

    x y

--- content.c
+++ content.c
@@ -3,8 +3,14 @@
 void		content(t_env *env)
 {
 	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
+//	glClear(GL_COLOR_BUFFER_BIT);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0.0f, WIDTH, HEIGHT, 0.0f, 0.0f, 1.0f);
+
 	key_hook(env);
+	//	move_ball(env);
 	draw(env);
 }
